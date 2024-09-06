d6f390938098bb8b78cb21fe3c2cd9d3b3a4e647
Saad Khan <saad.khan955@gmail.com>
2024-09-06 19:33:20 +0530

    Added Toolbar.

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,12 +1,15 @@
-interface WorkspaceIdPageProps {
-  params: {
-    workspaceId: string
-  }
-}
+"use client"
 
-const WorkspaceIdPage = ({ params }: WorkspaceIdPageProps) => {
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
+import { json } from "stream/consumers";
+
+
+const WorkspaceIdPage = () => {
+  const workspaceId = useWorkspaceId()
+  const { data } = useGetWorkspace({ id: workspaceId })
   return (<div>
-    ID: {params.workspaceId}
+    Data: {JSON.stringify(data)}
   </div>);
 }
 
