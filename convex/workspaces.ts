7a053e22e21cfea2810b1f8dc739c4d80ca1084b
Saad Khan <saad.khan955@gmail.com>
2024-09-09 16:51:45 +0530

    Added the ability to create more workspaces.

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -2,6 +2,15 @@ import { mutation, query } from "./_generated/server";
 import { v } from "convex/values"
 import { auth } from "./auth";
 
+const generateCode = () => {
+  const code = Array.from(
+    { length: 6 },
+    () => "0123456789abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random() * 36)]
+  ).join("")
+
+  return code
+}
+
 export const create = mutation({
   args: {
     name: v.string()
@@ -13,12 +22,18 @@ export const create = mutation({
       throw new Error("Unauthorized")
     }
 
-    const joinCode = "123456"
+    const joinCode = generateCode()
 
     const workspaceId = await ctx.db.insert("workspaces", {
       name: args.name,
       userId,
       joinCode
+    })
+
+    await ctx.db.insert("members", {
+      userId,
+      workspaceId,
+      role: "admin"
     })
 
     return workspaceId
@@ -28,7 +43,29 @@ export const create = mutation({
 export const get = query({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db.query("workspaces").collect()
+    const userId = await auth.getUserId(ctx)
+    if (!userId) {
+      return []
+    }
+
+    const members = await ctx.db
+      .query("members")
+      .withIndex("by_user_id", (q) => q.eq("userId", userId))
+      .collect()
+
+    const workspaceIds = members.map((member) => member.workspaceId)
+
+    const workspaces = []
+
+    for (const workspaceId of workspaceIds) {
+      const workspace = await ctx.db.get(workspaceId)
+
+      if (workspace) {
+        workspaces.push(workspace)
+      }
+    }
+
+    return workspaces
   }
 })
 
@@ -39,6 +76,17 @@ export const getById = query({
 
     if (!userId) {
       throw new Error("Unauthorized")
+    }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.id).eq("userId", userId)
+      )
+      .unique()
+
+    if (!member) {
+      return null
     }
 
     return await ctx.db.get(args.id)
