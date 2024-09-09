4d20b608d376ab382935c8be96894ff5735266e5
Saad Khan <saad.khan955@gmail.com>
2024-09-09 16:20:54 +0530

    Added Sidebar.

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,16 +1,21 @@
 "use client"
 
 import { Toolbar } from "./toolbar";
+import { Sidebar } from "./sidebar";
 
 interface WorkspaceIdLayoutProps {
   children: React.ReactNode
 }
 
 const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
-  return (<div className="h-full">
-    <Toolbar />
-    {children}
-  </div>);
+  return (
+    <div className="h-full">
+      <Toolbar />
+      <div className="flex h-[calc(100vh-40px)]">
+        <Sidebar />
+        {children}
+      </div>
+    </div>);
 }
 
 export default WorkspaceIdLayout;
