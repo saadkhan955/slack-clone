03de148fe911a61ee64cf9c641feadb7532228e4
Saad Khan <saad.khan955@gmail.com>
2024-10-01 21:15:08 +0530

    WIP-Created channel page

---
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -0,0 +1,7 @@
+const ChannelIdPage = () => {
+  return (
+    <div>Channel Id</div>
+  );
+}
+
+export default ChannelIdPage;
--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,16 +1,50 @@
 "use client"
+import { useMemo, useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { Loader, TriangleAlert } from "lucide-react";
 
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
 import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
-import { json } from "stream/consumers";
+import { useCreateChannelModal } from "@/features/channels/store/use-create-channel-modal";
+import { UseGetChannels } from "@/features/channels/api/use-get-channels";
 
 
 const WorkspaceIdPage = () => {
   const workspaceId = useWorkspaceId()
-  const { data } = useGetWorkspace({ id: workspaceId })
-  return (<div>
-    Data: {JSON.stringify(data)}
-  </div>);
+  const router = useRouter()
+  const [open, setOpen] = useCreateChannelModal()
+
+  const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({ id: workspaceId })
+  const { data: channels, isLoading: channelsLoading } = UseGetChannels({ workspaceId })
+
+  const channelId = useMemo(() => channels?.[0]?._id, [channels])
+
+  useEffect(() => {
+    if (workspaceLoading || channelsLoading || !workspace) return
+    if (channelId) {
+      router.push(`/workspace/${workspaceId}/channel/${channelId}`)
+    } else if (!open) {
+      setOpen(true)
+    }
+  }, [channelId, workspaceLoading, channelsLoading, workspace, router, workspaceId, open, setOpen])
+
+  if (workspaceLoading || channelsLoading) {
+    return (
+      <div className="h-full flex-1 flex items-center justify-center flex-col gap-2">
+        <Loader className="size-6 animate-spin text-muted-foreground" />
+      </div>
+    )
+  }
+  if (!workspace) {
+    return (
+      <div className="h-full flex-1 flex items-center justify-center flex-col gap-2">
+        <TriangleAlert className="size-6 animate-spin text-muted-foreground" />
+        <span className="text-sm text-muted-foreground">Workspace not found</span>
+      </div>
+    )
+  }
+
+  return null
 }
 
 export default WorkspaceIdPage;
