70647f13691e99567fd1254916c732d33dfb7615
Saad Khan <saad.khan955@gmail.com>
2024-11-14 15:52:49 +0530

    WIP - Editor

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -1,11 +1,26 @@
-import Editor from "@/components/editor"
+import dynamic from "next/dynamic";
+import Quill from "quill";
+import { useRef } from "react";
 
-export const ChatInput = () => {
-  return (
-    <div
-      className="px-5 w-full"
-    >
-      <Editor />
-    </div>
-  )
+const Editor = dynamic(() => import("@/components/editor"), { ssr: false });
+
+
+interface ChatInputProps {
+  placeholder: string
 }
+
+export const ChatInput = ({ placeholder }: ChatInputProps) => {
+
+  const editorRef = useRef<Quill | null>(null);
+
+  return (
+    <div className="px-5 w-full">
+      <Editor
+        placeholder={placeholder}
+        onSubmit={() => { }}
+        disabled={false}
+        innerRef={editorRef}
+      />
+    </div>
+  );
+};
