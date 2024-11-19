205e690c35840a1fa8397e59af0e4a790d3b9ce9
Saad Khan <saad.khan955@gmail.com>
2024-11-19 15:58:17 +0530

    Final schema.

--- convex/messages.ts
+++ convex/messages.ts
@@ -19,6 +19,7 @@ export const create = mutation({
         image: v.optional(v.id("_storage")),
         workspaceId: v.id("workspaces"),
         channelId: v.optional(v.id("channels")),
+        conversationId: v.optional(v.id("conversations")),
         parentMessageId: v.optional(v.id("messages"))
     },
     handler: async (ctx, args) => {
@@ -34,11 +35,24 @@ export const create = mutation({
             throw new Error("Unauthorized");
         }
 
+        let _conversationId = args.conversationId;
+
+        if (!args.conversationId && args.channelId && args.parentMessageId) {
+            const parentMessage = await ctx.db.get(args.parentMessageId);
+
+            if (!parentMessage) {
+                throw new Error("Parent message not found");
+            }
+
+            _conversationId = parentMessage.conversationId;
+        }
+
         const messageId = await ctx.db.insert("messages", {
             memberId: member._id,
             body: args.body,
             image: args.image,
             channelId: args.channelId,
+            conversationId: _conversationId,
             workspaceId: args.workspaceId,
             parentMessageId: args.parentMessageId,
             updatedAt: Date.now(),
